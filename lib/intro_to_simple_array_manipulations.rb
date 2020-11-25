984db9c5c9b47bb19fc6ffa64d6f8fc8336169a3
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-25 02:29:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array, string)
-  array = ["white", "blue", "red", "green", "yellow", "orange", "pink"]
-  next_array = "violet"
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
   array.push(string)
 end
 
