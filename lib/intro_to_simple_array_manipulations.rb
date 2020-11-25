5997e11a961d9cba36c0c6c9ffc55431f444e833
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-25 02:27:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array = ["white", "blue", "red", "green", "yellow", "orange", "pink"]
+  next_array = "violet"
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array = ["Africa", "Asia", "Europe", "North America", "Australia", "Antarctica"]
+  array.unshift("South America")
+end
+
+def using_pop(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Australia", "Antarctica"]
+end
+
+def pop_with_args(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Australia", "Antarctica"]
+end
+
+def using_shift(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Australia", "Antarctica"]
+end
+
+def shift_with_args(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Australia", "Antarctica"]
+
+end
